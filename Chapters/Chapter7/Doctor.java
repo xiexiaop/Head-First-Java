830faedfc66a7d7012dd56eef2a6625901478f1f
Zac <13947123462@163.com>
2019-01-20 22:22:22 +0800

    20190120
    JAVA_Head_First_Chapter7
    Class Inheritage

---
+++ Chapters/Chapter7/Doctor.java
@@ -0,0 +1,22 @@
+// 继承实例
+class Doctor{
+    Boolean worksAtHospital;
+    void treatPatient(){
+    }
+}
+
+class FamilyDoctor extends Doctor{
+    boolean makesHouseCalls;
+    void giveAdvice(){
+
+    }
+}
+
+class surgon extends Doctor{
+    void treatPatient(){
+    }
+
+    void makeIncision(){
+        
+    }
+}
